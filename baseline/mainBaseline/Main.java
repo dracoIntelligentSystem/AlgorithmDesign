e9e0bc24fc08c0416f0a72e5c8df2fb662c62d14
A.Balestrucci <draco_@hotmail.it>
2017-04-11 09:52:12 +0200

    First commit!

---
+++ baseline/mainBaseline/Main.java
@@ -0,0 +1,10 @@
+package mainBaseline;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello in Baseline!!");
+
+	}
+
+}
---
+++ hw_AlexLuca/mainHW/MainHW.java
@@ -0,0 +1,10 @@
+package mainHW;
+
+public class MainHW {
+
+	public static void main(String[] args) {
+		System.out.println("Hello in HW Alex & Luca!!");
+
+	}
+
+}
