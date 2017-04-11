dcd03a9b427b7898437bdc500cc88c1f4ed34647
A.Balestrucci <draco_@hotmail.it>
2017-04-11 14:45:10 +0200

    bla

--- baseline/businessLogic/Evaluations.java
+++ baseline/businessLogic/Evaluations.java
@@ -6,7 +6,7 @@ import weka.classifiers.Evaluation;
 public class Evaluations {
 
 	public static void evaluateModel(Classifier classifier){
-//		Evaluation eval = new Evaluation(newData);
-//		 eval.crossValidateModel(tree, newData, 10, new Random(1));
+		Evaluation eval = new Evaluation(newData);
+		 eval.crossValidateModel(classifier, arg1, arg2, arg3, arg4);
 	}
 }
