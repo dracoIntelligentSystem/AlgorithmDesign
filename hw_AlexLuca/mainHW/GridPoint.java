f36d751920374e44dbc6ce2ba4bd74e40a06de36
Luca Di Stefano <luca.distefano@gssi.it>
2017-04-11 14:50:10 +0200

    Add Metrics member to GridPoint
    computeMetrics() in the future will build the
    C4.5 tree and compute relevant metrics of
    accuracy

--- hw_AlexLuca/mainHW/GridPoint.java
+++ hw_AlexLuca/mainHW/GridPoint.java
@@ -3,9 +3,18 @@ package mainHW;
 public class GridPoint {
 	public double m;
 	public double c;
+	private Metrics metrics = null;
 	
 	public GridPoint(double m, double c){
 		this.m = m;
 		this.c = c;
 	}
+
+	public void computeMetrics() {
+		this.metrics = new Metrics();
+	}
+	public Metrics getMetrics() {
+		return metrics;
+	}
+
 }
