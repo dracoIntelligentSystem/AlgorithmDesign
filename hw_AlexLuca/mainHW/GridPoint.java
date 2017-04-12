9c34aba3b64b9dad5faaa809ca378f4b854e7e91
Luca Di Stefano <luca.distefano@gssi.it>
2017-04-12 18:57:32 +0200

    Javadoc + cleanup

--- hw_AlexLuca/mainHW/GridPoint.java
+++ hw_AlexLuca/mainHW/GridPoint.java
@@ -6,14 +6,27 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 
+
+/**
+ * Represents a point in the search space.
+ * <p>
+ * Also contain the logic to instantiate/evaluate
+ * a C4.5 tree at the given coordinates, with the
+ * given test set.
+ * </p>
+ */
 public class GridPoint {
 	public int m;
 	public float c;
 	private Instances data;
 	public double accuracy;
-	
-	//private Metrics metrics = null;
-	
+		
+	/**
+	 * Creates a new GridPoint.
+	 * @param m Minimum number of instances per leaf.
+	 * @param c Confidence threshold for pruning.
+	 * @param data Test set to evaluate the tree. 
+	 */
 	public GridPoint(int m, float c, Instances data){
 		this.m = m;
 		this.c = c;
@@ -22,17 +35,20 @@ public class GridPoint {
 
 	public void computeMetrics() throws Exception {
 		Evaluation ev = new Evaluation(this.data);
-	
 		ev.crossValidateModel(buildTree(), data, 10, new Random(1));
-		
-		//this.metrics = new Metrics();
 		this.accuracy = ev.correct()/(ev.correct() + ev.incorrect());
 	}
-	
-//	public Metrics getMetrics() {
-//		return metrics;
-//	}
 
+	/**
+	 * Builds a C4.5 tree for the current coordinates.
+	 * 
+	 * <p>
+	 * <code>J48</code> is the Weka implementation of the
+	 * C4.5 algorithm.
+	 * </p>
+	 * @return A tree whose parameters correspond to the
+	 * coordinates of <code>this</code>.
+	 */
 	private J48 buildTree(){
 		J48 tree = new J48();
 		tree.setConfidenceFactor(c);
