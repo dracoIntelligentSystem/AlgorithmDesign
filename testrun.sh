d53589a02efb1254986d4b82fb1d8bfa157e488b
Luca Di Stefano <luca.distefano@gssi.it>
2017-05-09 09:54:51 +0200

    Final test run script + changed wine-quality set
    Wine-quality label class is now nominal

--- testrun.sh
+++ testrun.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 WEKAJAR=/Applications/weka-3-6-15-oracle-jvm.app/Contents/Java/weka.jar
 JVMARGS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=duration=10s,filename="
-OUTPUTDIR="/Users/luca/Desktop"
+OUTPUTDIR="/Users/luca/Desktop/out"
 
 
-GRIDPARAMS="1 10 1 0.1 0.5 0.01"
+GRIDPARAMS=("1" "10" "1" "0.1" "0.5" "0.01")
 
 cd bin/
 
-# for i in {1..5}
-# do
-#     FILENAME="filename=$OUTPUTDIR/mainHW$i.jfr"
-#     java $JVMARGS$FILENAME -cp $WEKAJAR:. mainHW/MainHW "../data/iris.arff" 1 10 1 0.1 0.5 0.01 >> $OUTPUTDIR/mainHW.txt
-# done
+declare -a CLASSES=("mainHW/MainHW" "mainHW_sequential/MainHW" "mainBaseline/Main")
+declare -a DATASETS=("iris" "wine-quality" "magic04")
 
-FILENAME="$OUTPUTDIR/mainHW"
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainHW/MainHW "../data/iris.arff" $GRIDPARAMS >> $FILENAME.txt
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainHW/MainHW "../data/magic.arff" $GRIDPARAMS >> $FILENAME.txt
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainHW/MainHW "../data/wine-qualities.arff" $GRIDPARAMS >> $FILENAME.txt
-
-FILENAME="$OUTPUTDIR/mainHW_sequential"
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainHW_sequential/MainHW "../data/iris.arff" $GRIDPARAMS >> $FILENAME.txt
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainHW_sequential/MainHW "../data/magic.arff" $GRIDPARAMS >> $FILENAME.txt
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainHW_sequential/MainHW "../data/wine-qualities.arff" $GRIDPARAMS >> $FILENAME.txt
-FILENAME="$OUTPUTDIR/mainBaseline"
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainBaseline/Main "../data/iris.arff" $GRIDPARAMS >> $FILENAME.txt
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainBaseline/Main "../data/magic.arff" $GRIDPARAMS >> $FILENAME.txt
-java $JVMARGS$FILENAME.jfr -cp $WEKAJAR:. mainBaseline/Main "../data/wine-qualities.arff" $GRIDPARAMS >> $FILENAME.txt
+for TESTCLASS in "${CLASSES[@]}"
+do
+    echo $TESTCLASS
+    FILENAME=$OUTPUTDIR/`dirname $TESTCLASS`
+    for DATASET in "${DATASETS[@]}"
+    do
+        java $JVMARGS$FILENAME-$DATASET.jfr -cp $WEKAJAR:. $TESTCLASS "../data/$DATASET.arff" ${GRIDPARAMS[@]} >> $FILENAME.txt
+    done
+done
